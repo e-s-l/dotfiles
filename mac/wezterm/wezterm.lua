498951f4c54ddeac97a99592cfc8acee7c00598b
pearl <pearl@pearls-MacBook-Pro.local>
2025-01-21 22:36:24 +1100

    setting up mac dotfiles

---
+++ mac/wezterm/wezterm.lua
@@ -0,0 +1,41 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+--
+
+config.color_scheme = 'Japanesque'
+
+-----------
+
+-- returns color scheme dependant on operating system theme setting (dark/light)
+local function color_scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Tokyo Night"
+  else
+    return "Tokyo Night Light (Gogh)"
+  end
+end
+
+
+--
+
+-- Pull in WezTerm API
+local wezterm = require 'wezterm'
+
+-- Initialize actual config
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Appearance
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 12.0
+--config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = false
+
+--------------------
+
+--
+return config
