aed4cc4c0d111dda595125df2ddcc3e0cee88373
pearl <pearl@pearls-MacBook-Pro.local>
2025-01-24 08:20:26 +1100

    start on the wezterm

--- mac/wezterm/wezterm.lua
+++ mac/wezterm/wezterm.lua
@@ -1,41 +1,39 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
---
 
-config.color_scheme = 'Japanesque'
-
------------
+-- Prelim's
 
 -- returns color scheme dependant on operating system theme setting (dark/light)
 local function color_scheme_for_appearance(appearance)
   if appearance:find "Dark" then
-    return "Tokyo Night"
+    return "Japanesque"
   else
     return "Tokyo Night Light (Gogh)"
   end
 end
 
+-- Init
 
 --
-
--- Pull in WezTerm API
-local wezterm = require 'wezterm'
-
--- Initialize actual config
 local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Appearance
+-- Wezterm API:
+local wezterm = require 'wezterm'
+
+-- Config
+
+--
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 12.0
---config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+
+--
+config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+
+--
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = false
-
---------------------
 
 --
 return config
